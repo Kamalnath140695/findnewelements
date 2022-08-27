bb90b4dff1178d2efab1f9a27874b33e17b5d676
Kamalnath <kamalnathm@outlook.com>
2022-08-27 07:12:59 +0530

    initial

---
+++ src/WebElements/InteractionOfWebElements.java
@@ -0,0 +1,21 @@
+package WebElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InteractionOfWebElements {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\New folder\\FindWebElements\\dRIVER\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("kamalnath"+Keys.ENTER);
+	}
+
+}
